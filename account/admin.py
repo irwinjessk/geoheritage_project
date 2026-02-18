5d064226ae009e31b3a877243b5145a1c52788d0
Irwin Djriga <irwindjriga@gmail.com>
2026-02-18 19:52:57 +0000

    Add models to Django admin for CRUD functionality

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models.user_model import User
+from .models.role_model import Role
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'phone', 'is_staff', 'is_superuser', 'created_at')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'created_at')
+    search_fields = ('username', 'email', 'phone')
+    readonly_fields = ('created_at', 'updated_at')
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'address')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined', 'created_at', 'updated_at')}),
+        ('Roles', {'fields': ('roles',)}),
+    )
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level', 'created_at')
+    list_filter = ('level', 'created_at')
+    search_fields = ('name', 'description')
+    readonly_fields = ('created_at',)
+    ordering = ('level',)
--- heritage/admin.py
+++ heritage/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Patrimoine
 
-# Register your models here.
+
+@admin.register(Patrimoine)
+class PatrimoineAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'type', 'ville', 'created_by', 'created_at')
+    list_filter = ('type', 'ville', 'created_at')
+    search_fields = ('nom', 'description', 'ville')
+    readonly_fields = ('created_at', 'updated_at')
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(created_by=request.user)
+    
+    def save_model(self, request, obj, form, change):
+        if not change:  # Si c'est une création
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
